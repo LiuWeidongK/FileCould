4ad5895605d5e54a8b832badab70645cc5690bf5
LiuWeidon <570520093@qq.com>
2017-12-25 22:32:43 +0800

    2017/12/25 commits

--- create.php
+++ create.php
@@ -4,7 +4,7 @@ $dir = $_REQUEST["dir"];
 $dirName = pathinfo($dir)["dirname"];
 $option = iconv("UTF-8", "GBK", $dir);
 if (!file_exists($option)){
-    mkdir ($option,0777,true);
+    mkdir($option,0777,true);
     header("location: index.php?dir=" . $dirName);
 } else {
     header("location: index.php?error=folderRepeat&dir=" . $dirName);
